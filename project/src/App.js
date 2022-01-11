108bcc7bc64e9544824f94267d2ca4506eee22c2
Madalik <73160318+Madalik@users.noreply.github.com>
2022-01-11 10:01:43 +0200

    /

--- project/src/App.js
+++ project/src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Footer from "./components/Footer";
